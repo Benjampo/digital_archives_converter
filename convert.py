93fa3e3ed777e222ecd9388d87bf0a0865b9b831
Benjampo <benjaminporchet.pro@gmail.com>
2024-09-20 09:50:59 +0200

    update snake

--- convert.py
+++ convert.py
@@ -10,6 +10,7 @@ from helpers.converters.videos import convert_videos
 from helpers.converters.text import convert_text
 from helpers.converters.mkv import convert_to_mkv
 from helpers.delete_empty_folders import delete_empty_folders
+from helpers.to_snake_case import to_snake_case
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -28,6 +29,13 @@ def convert_folder(source_folder, destination_folder=None):
         print(f"Cloning source folder to: {destination_folder}")
         shutil.copytree(source_folder, destination_folder)
         print(f"Cloned source folder to: {destination_folder}")
+
+    for root, dirs, files in os.walk(destination_folder):
+        logging.info(f"Renaming files to camel case in {root}")
+        for file in files:
+            logging.info(f"Renaming {file} to {to_snake_case(file)}")
+            new_name = to_snake_case(file)
+            os.rename(os.path.join(root, file), os.path.join(root, new_name))
 
     # Walk through the cloned folder
     for root, dirs, files in os.walk(destination_folder):
--- helpers/to_camel_case.py
+++
@@ -1,4 +0,0 @@
-def to_camel_case(snake_str):
-    snake_str = snake_str.lower() 
-    components = snake_str.split('_')
-    return components[0] + ''.join(x.title() for x in components[1:])
