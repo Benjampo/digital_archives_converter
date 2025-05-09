72c454c9ad00a5223babbe2f5ba4becdcb64c696
Benjampo <benjaminporchet.pro@gmail.com>
2025-05-09 14:19:51 +0200

    Revert "lint"
    This reverts commit 2b21e87269bca32b075d118b066897fdd12a1d35.

--- archives_converter/helpers/name_identifier.py
+++ archives_converter/helpers/name_identifier.py
@@ -1,29 +1,25 @@
-from config.ignore import (
-    image_extensions,
-    video_extensions,
-    audio_extensions,
-    text_extensions,
-)
-
-
+from config.ignore import image_extensions, video_extensions, audio_extensions, text_extensions
 def predict_name_based_on_extension(input_name, convert_type):
-    # get file extension
-    extension = "." + input_name.split(".")[-1]
+   #get file extension
+   extension = "." + input_name.split(".")[-1]
 
-    # replace extension based on the conversion
-    if extension in image_extensions and convert_type == "AIP":
-        input_name = input_name.replace(extension, "_tiff.tiff")
-    elif extension in image_extensions and convert_type == "DIP":
-        input_name = input_name.replace(extension, "_jpg.jpg")
-    elif extension in video_extensions and convert_type == "AIP":
-        input_name = input_name.replace(extension, "_ffv1.mkv")
-    elif extension in video_extensions and convert_type == "DIP":
-        input_name = input_name.replace(extension, "_mp4.mp4")
-    elif extension in audio_extensions and convert_type == "AIP":
-        input_name = input_name.replace(extension, "_wav.wav")
-    elif extension in audio_extensions and convert_type == "DIP":
-        input_name = input_name.replace(extension, "_mp3.mp3")
-    elif extension in text_extensions:
-        input_name = input_name.replace(extension, "_pdfa.pdf")
+   # replace extension based on the conversion
+   if extension in image_extensions and convert_type == "AIP":
+       input_name = input_name.replace(extension, "_tiff.tiff")
+   elif extension in image_extensions and convert_type == "DIP":
+       input_name = input_name.replace(extension, "_jpg.jpg")
+   elif extension in video_extensions and convert_type == "AIP":
+       input_name = input_name.replace(extension, "_ffv1.mkv")
+   elif extension in video_extensions and convert_type == "DIP":
+       input_name = input_name.replace(extension, "_mp4.mp4")
+   elif extension in audio_extensions and convert_type == "AIP":
+       input_name = input_name.replace(extension, "_wav.wav")
+   elif extension in audio_extensions and convert_type == "DIP":
+       input_name = input_name.replace(extension, "_mp3.mp3")
+   elif extension in text_extensions :
+       input_name = input_name.replace(extension, "_pdfa.pdf")
+   else:
+       print("No conversion needed")
 
-    return input_name
+   
+   return input_name
