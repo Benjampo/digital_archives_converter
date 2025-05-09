29e697d460aa2e3942e20c8cd80fae10bca40df4
Benjampo <benjaminporchet.pro@gmail.com>
2025-05-09 14:18:48 +0200

    revert

--- archives_converter/utils/rename.py
+++ archives_converter/utils/rename.py
@@ -1,9 +1,17 @@
 import os
 from helpers.to_snake_case import to_snake_case
 from helpers.folders import count_files_and_folders
-from rich.progress import Progress, BarColumn, TextColumn, TimeRemainingColumn, SpinnerColumn
+from rich.progress import (
+    Progress,
+    BarColumn,
+    TextColumn,
+    TimeRemainingColumn,
+    SpinnerColumn,
+)
 from rich import print
 from config.ignore import text_files_to_ignore
+
+
 def rename_files_and_folders(folder, selected_media_types):
     print("[bold cyan]Starting renaming files and folders[/bold cyan] :pencil2:")
 
@@ -12,16 +20,24 @@ def rename_files_and_folders(folder, selected_media_types):
         TextColumn("[progress.description]{task.description}"),
         BarColumn(),
         TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
-        TimeRemainingColumn()
+        TimeRemainingColumn(),
     ) as progress:
-        total_files, total_folders = count_files_and_folders(folder, selected_media_types)
+        total_files, total_folders = count_files_and_folders(
+            folder, selected_media_types
+        )
         total_items = total_files + total_folders
-        rename_task = progress.add_task("[bold blue]Renaming items...[/bold blue]", total=total_items)
+        rename_task = progress.add_task(
+            "[bold blue]Renaming items...[/bold blue]", total=total_items
+        )
 
         for root, dirs, files in os.walk(folder, topdown=False):
             # Rename files
             for file in files:
-                if file == '.DS_Store' or file.startswith('.') or file in text_files_to_ignore:  # Skip .DS_Store files and hidden files
+                if (
+                    file == ".DS_Store"
+                    or file.startswith(".")
+                    or file in text_files_to_ignore
+                ):  # Skip .DS_Store files and hidden files
                     continue
                 new_name = to_snake_case(file)
                 os.rename(os.path.join(root, file), os.path.join(root, new_name))
@@ -29,7 +45,7 @@ def rename_files_and_folders(folder, selected_media_types):
 
             # Rename folders
             for dir in dirs:
-                if dir == 'VIDEO_TS' or file.startswith('.'):  # Skip VIDEO_TS folders
+                if dir == "VIDEO_TS" or file.startswith("."):  # Skip VIDEO_TS folders
                     progress.advance(rename_task)
                     continue
                 new_name = to_snake_case(dir)
