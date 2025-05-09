9f7dbace253678496520c6412aae8c7526d3bed8
Benjampo <benjaminporchet.pro@gmail.com>
2025-05-09 14:10:37 +0200

    bag-it info

--- archives_converter/helpers/bagit.py
+++ archives_converter/helpers/bagit.py
@@ -3,6 +3,7 @@ import pkg_resources
 import mimetypes
 from datetime import datetime
 
+
 def detect_formats(bag_dir):
     """Detects the file format types in a directory."""
     formats = set()
@@ -13,8 +14,9 @@ def detect_formats(bag_dir):
             mime_type, _ = mimetypes.guess_type(file)
             if mime_type:
                 formats.add(mime_type)
-    
+
     return ", ".join(formats)
+
 
 def format_bag_size(size_in_bytes):
     """Converts the bag size to MB or GB, depending on its size."""
@@ -29,7 +31,9 @@ def format_bag_size(size_in_bytes):
 def create_bagit_txt(bag_dir):
     """Creates or updates the bagit.txt file with necessary information."""
     bagit_txt_path = os.path.join(bag_dir, "bagit.txt")
-    bagit_version = ".".join(pkg_resources.get_distribution("bagit").version.split(".")[:2])  # Get BagIt version, only MAJOR.MINOR otherwise it will throw error
+    bagit_version = ".".join(
+        pkg_resources.get_distribution("bagit").version.split(".")[:2]
+    )  # Get BagIt version, only MAJOR.MINOR otherwise it will throw error
 
     bagit_txt_content = f"""BagIt-Version: {bagit_version}
 Version: 1.0 
@@ -42,16 +46,24 @@ Tag-File-Character-Encoding: UTF-8
     except Exception as e:
         print(f"❌ Error creating bagit.txt: {e}")
 
+
 def create_bag_info(bag_dir):
     """Creates or updates the bag-info.txt file in the BagIt with specific information."""
     bag_info_path = os.path.join(bag_dir, "bag-info.txt")
     creation_date = datetime.now().strftime("%Y-%m-%d")
     bag_name = os.path.basename(bag_dir)
-    bag_size = sum(os.path.getsize(os.path.join(root, file)) for root, dirs, files in os.walk(bag_dir) for file in files)
+    bag_size = sum(
+        os.path.getsize(os.path.join(root, file))
+        for root, dirs, files in os.walk(bag_dir)
+        for file in files
+    )
     formatted_bag_size = format_bag_size(bag_size)
     formats = detect_formats(bag_dir)  # Format detection
-    bagit_version = pkg_resources.get_distribution("bagit").version # Get BagIt version
-    total_files = sum(1 for root, dirs, files in os.walk(bag_dir) for file in files) # Count the number of files in the bag
+    bagit_version = pkg_resources.get_distribution("bagit").version  # Get BagIt version
+    total_files = sum(
+        1 for root, dirs, files in os.walk(bag_dir) for file in files
+    )  # Count the number of files in the bag
+
     bag_info_content = f"""Bag-Name: {bag_name}
         Bag-Size: {formatted_bag_size}
         Creation-Date: {creation_date}
@@ -64,6 +76,26 @@ def create_bag_info(bag_dir):
         Number-of-Files: {total_files}
         Checksum-Algorithm: SHA-256
     """
+
+    if "bag-info.txt" in os.listdir(bag_dir):
+        bag_info_path = os.path.join(bag_dir, "bag-info.txt")
+        bag_dir_files = os.listdir(bag_dir)
+        formatted_bag_size = format_bag_size(bag_size)
+        modification_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        bagit_version = pkg_resources.get_distribution("bagit").version
+        try:
+            with open(bag_info_path, "a") as f:
+                f.write(f"\n--- Mise à jour du {modification_date} ---\n")
+                f.write(f"BagIt-Version: {bagit_version}\n")
+                f.write(f"Bag-Size: {formatted_bag_size}\n")
+                f.write(f"Nombre de fichiers ajoutés: {len(bag_dir_files)}\n")
+
+                for file in bag_dir_files:
+                    f.write(f"- {file}\n")
+
+            print(f"✅ bag-info.txt mis à jour avec {len(bag_dir_files)} fichiers")
+        except Exception as e:
+            print(f"❌ Erreur lors de la mise à jour de bag-info.txt : {e}")
 
     try:
         with open(bag_info_path, "w") as f:
