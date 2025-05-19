bd5ce093a990c43467a6adfd3deae60b6525a91c
Benjampo <benjaminporchet.pro@gmail.com>
2025-05-19 10:42:18 +0200

    finalize

--- archives_converter/helpers/to_snake_case.py
+++ archives_converter/helpers/to_snake_case.py
@@ -4,12 +4,20 @@ import os
 from config.ignore import text_files_to_ignore
 
 
-def to_snake_case(file_name):
+def to_snake_case(file_path):
     # ignore files existing
-    if any(ignore_str in file_name for ignore_str in text_files_to_ignore):
-        return file_name
+    if any(ignore_str in file_path for ignore_str in text_files_to_ignore):
+        splitted_path = file_path.split("/")
+        if len(splitted_path) > 1:
+            made_parts = [
+                to_snake_case(splitted_path) for splitted_path in splitted_path
+            ]
+            return "/".join(made_parts)
+
+        return file_path
+
     else:
-        name, ext = os.path.splitext(file_name)
+        name, ext = os.path.splitext(file_path)
 
         snake_str = (
             unicodedata.normalize("NFKD", name)
